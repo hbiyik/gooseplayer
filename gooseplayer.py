8e2108d65d4404850f78fd5abc1d33d049366875
boogiepop <boogiepop@gmx.com>
2021-07-05 14:17:27 +0200

    initial

---
+++ gooseplayer.py
@@ -0,0 +1,234 @@
+from pcapng import FileScanner
+import logging
+import time
+import sys
+import threading
+import platform
+import socket
+import queue
+import argparse
+import os
+
+
+ISLIN = "linux" in platform.system().lower()
+DEFACC = 0.01
+
+if not ISLIN:
+    from scapy import sendrecv
+    from scapy import interfaces
+
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger("GooseSim")
+
+
+class GooseSim:
+    def __init__(self, filename, srcmac=None, gooseappid=None):
+        self.deltat = None
+        self.firstts = None
+        self.lana = []
+        self.lanb = []
+        self.nonred = []
+        self.threada = None
+        self.threadb = None
+        self.totaldelaya = 0
+        self.totaldelayb = 0
+        self.logqueue = queue.Queue()
+        with open(filename, 'rb') as fp:
+            scanner = FileScanner(fp)
+            for block in scanner:
+                try:
+                    data = block.packet_data
+                except AttributeError:
+                    continue
+                src = data[0:6]
+                # dst = data[6:12]
+                typ = data[12:14]
+                if srcmac and srcmac != src:
+                    continue
+                if typ == b"\x88\xb8": # goose
+                    appid = data[14:16]
+                    if gooseappid and gooseappid != appid:
+                        continue
+                    pdulen = int.from_bytes(data[16:18], "big")
+                    endpdu = 14 + pdulen
+                    suffixlen = len(data) - endpdu
+                    if suffixlen == 6:
+                        # seqnr = data[endpdu: endpdu + 2]
+                        lanid =  int.from_bytes(data[endpdu + 2: endpdu + 3], "big") >> 4
+                        suffix = data[endpdu + 4: endpdu + 6]
+                        if b"\x88\xfb" == suffix:
+                            #PRP:
+                            if lanid == 10:
+                                self.addgoose(block.timestamp, data, self.lana)
+                            else:
+                                self.addgoose(block.timestamp, data, self.lanb)
+                    else:
+                        pass
+                        # non redundant gooses
+                        
+    def addgoose(self, timestamp, data, target):
+        if not self.firstts or timestamp < self.firstts:
+            self.firstts = timestamp
+            logger.info("First Timestamp marked: %s", self.firstts)
+        target.append((timestamp, data))
+        
+    def logger(self):
+        stop = 0
+        while True:
+            if stop == 2:
+                break
+            channel, deltat, packet = self.logqueue.get()
+            if channel is None:
+                stop += 1
+                continue
+            pdu = list(self.parsegoosepdu(packet))
+            appid = self._byte2int(packet[14:16])
+            sq = pdu.pop()[1]
+            st = pdu.pop()[1]
+            logger.warning("%s: Delayed Packet %04.fms appid:%03i st:%04i sq:%04i pdu:%s",
+                           channel,
+                           deltat * 1000,
+                           appid,
+                           st,
+                           sq,
+                           " ".join("%s:%s" % (x, y) for x, y in pdu))
+            
+            
+    
+    def writer(self, packets, iface, ischana=True, accuracy=DEFACC):
+        if ischana:
+            channel = "Channel A"
+        else:
+            channel = "Channel B"
+        sender = Sender(iface)
+        totaldelay = 0
+        for timestamp, packet in packets:
+            if not self.deltat:
+                while True:
+                    if timestamp == self.firstts:
+                        self.deltat = time.time() - timestamp
+                        logger.info("%s: Clock Synched: first ts of capture: %s, Actual deltat: %s s",
+                                    channel,
+                                    self.firstts,
+                                    self.deltat)
+                        break
+                    elif self.deltat:
+                        break
+            targetts = timestamp + self.deltat + totaldelay
+            while True:
+                deltatpacket = time.time() - targetts
+                if deltatpacket >= 0:
+                    totaldelay += deltatpacket
+                    sender.send(packet)
+                    """
+                    # most likely this is slower
+                    threading.Thread(target=sender.send, args=(packet,)).start()
+                    """
+                    if deltatpacket >= accuracy:
+                        self.logqueue.put((channel, deltatpacket, packet))
+                    break
+        self.logqueue.put((None, None, None))
+        sender.close()
+        if ischana:
+            self.totaldelaya = totaldelay
+        else:
+            self.totaldelayb = totaldelay
+
+                        
+    def run(self, targetifa, targetifb, accuracy=DEFACC):
+        self.threada = threading.Thread(target=self.writer, args=(self.lana, targetifa, True, accuracy))
+        self.threadb = threading.Thread(target=self.writer, args=(self.lanb, targetifb, False, accuracy))
+        self.loggerthread = threading.Thread(target=self.logger)
+        self.threada.start()
+        self.threadb.start()
+        self.loggerthread.start()
+        self.threada.join()
+        self.threadb.join()
+        self.loggerthread.join()
+        
+    def _byte2int(self, h):
+        return int.from_bytes(h, byteorder="big")
+    
+    def _byte2ascii(self, h):
+        return h.decode()
+    
+    def _byte2ts(self, h):
+        seconds = int.from_bytes(h[:4], byteorder="big")
+        fracs = int.from_bytes(h[4:7], byteorder="big") + 1
+        return seconds + fracs / 16777216 # fractions / (2 ** 24)
+        
+    def parsegoosepdu(self, packet):
+        cursor = 24
+        while True:
+            if packet[cursor] == 128:
+                break
+            cursor += 1
+        for attr, callback in [("gcbref", self._byte2ascii), 
+                               ("tal", self._byte2int),
+                               ("datset", self._byte2ascii),
+                               ("goid", self._byte2ascii),
+                               ("timestamp", self._byte2ts),
+                               ("st", self._byte2int),
+                               ("sq", self._byte2int)]:
+            itemsize = self._byte2int(packet[cursor + 1:cursor + 2])
+            item = callback(packet[cursor + 2: cursor + 2 + itemsize])
+            cursor += itemsize + 2
+            yield attr, item
+        
+        
+class Sender:
+    def __init__(self, interface):
+        if ISLIN: 
+            self.socket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
+            self.socket.bind((interface, 0))
+        else:
+            found = None
+            for iface in interfaces.get_working_ifaces():
+                if interface.lower() == iface.name.lower():
+                    found = iface
+            if not found:
+                logger.error("Can not find interface %s", interface)
+                sys.exit()
+            self.iface = found
+    
+    def send(self, pkt):
+        if ISLIN:
+            self.socket.sendall(pkt)
+        else:
+            sendrecv.sendp(pkt, iface=self.iface, verbose=False)
+
+    def close(self):
+        if ISLIN:
+            self.socket.close()
+            
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Play goose telegrams over network interfaces')
+    parser.add_argument('filename', metavar='filename', type=str,  help='pcap file to process')
+    parser.add_argument('ifa', metavar='interface-a', type=str,  help='network interface name to play PRP Lan A Goose Telegrams')
+    parser.add_argument('ifb', metavar='interface-b', type=str,  help='network interface name to play PRP Lan A Goose Telegrams')
+    parser.add_argument('--appid', metavar='appid', type=int,  help='filter goose telgrams according to appid', default=None)
+    parser.add_argument('--srcmac', metavar='source mac address', type=str,  help='filter goose telegrams according source mac address', default=None)
+    parser.add_argument('--acc', metavar='accuracy', type=float,  help='raise a warning message when accuracy is above the threshold', default=DEFACC)
+    
+    args = parser.parse_args()
+    if not os.path.exists(args.filename):
+        logger.error("Filename: %s does not exist", args.filename)
+        sys.exit()
+        
+    if args.appid is not None:
+        args.appid = (args.appid).to_bytes(2, byteorder="big")
+        
+    if args.srcmac is not None:
+        args.srcmac = bytearray.fromhex(args.srcmac.replace(":", ""))
+
+    
+    sim = GooseSim(args.filename, args.srcmac, args.appid)
+    logger.info("Started Playing %s", args.filename)
+    sim.run(args.ifa, args.ifb, args.acc)
+    logger.info("Succesfully Played %s, LAN A: %s telegrams, LAN B: %s, telegrams ",
+                args.filename, len(sim.lana), len(sim.lanb))
+    logger.info("Total Delay for Channel A: %s ms", sim.totaldelaya * 1000)
+    logger.info("Total Delay for Channel B: %s ms", sim.totaldelayb * 1000)
---
+++ readme.md
@@ -0,0 +1,54 @@
+This tool opens a pcacpng file and replays the goose packets in it in resprective PRP networks.
+The LANA and LANB interfaces can be selected as regular network interface, so there is no need for a PRP enabled PC to use it.
+
+usage:
+
+```
+usage: gooseplayer.py [-h] [--appid appid] [--srcmac source mac address]
+                [--acc accuracy]
+                filename interface-a interface-b
+```
+				
+example:
+
+```
+python3 gooseplayer.py test.pcapng wi-fi wi-fi --appid 58 -acc 0.01
+```
+
+output:
+
+```
+INFO:GooseSim:First Timestamp marked: 1614003243.1967368
+INFO:GooseSim:Started Playing test.pcapng
+INFO:GooseSim:Channel A: Clock Synched: first ts of capture: 1614003243.1967368, Actual deltat: 11483018.281332493 s
+WARNING:GooseSim:Channel A: Delayed Packet 0010ms appid:058 st:2279 sq:0000 pdu:gcbref:M2E01_BEV1_LD0/LLN0$GO$gcb1 tal:11000 datset:M2E01_BEV1_LD0/LLN0$ds_gcb1 goid:M2E01_BEV1_LD0/LLN0gcb1 timestamp:1614003255.199737
+WARNING:GooseSim:Channel B: Delayed Packet 0011ms appid:058 st:2310 sq:0000 pdu:gcbref:M2E01_BEV1_LD0/LLN0$GO$gcb1 tal:11000 datset:M2E01_BEV1_LD0/LLN0$ds_gcb1 goid:M2E01_BEV1_LD0/LLN0gcb1 timestamp:1614003383.310439
+WARNING:GooseSim:Channel A: Delayed Packet 0011ms appid:058 st:2312 sq:0001 pdu:gcbref:M2E01_BEV1_LD0/LLN0$GO$gcb1 tal:11000 datset:M2E01_BEV1_LD0/LLN0$ds_gcb1 goid:M2E01_BEV1_LD0/LLN0gcb1 timestamp:1614003449.7344823
+WARNING:GooseSim:Channel A: Delayed Packet 0021ms appid:058 st:2314 sq:0000 pdu:gcbref:M2E01_BEV1_LD0/LLN0$GO$gcb1 tal:11000 datset:M2E01_BEV1_LD0/LLN0$ds_gcb1 goid:M2E01_BEV1_LD0/LLN0gcb1 timestamp:1614003449.7439027
+WARNING:GooseSim:Channel A: Delayed Packet 0025ms appid:058 st:2316 sq:0000 pdu:gcbref:M2E01_BEV1_LD0/LLN0$GO$gcb1 tal:11000 datset:M2E01_BEV1_LD0/LLN0$ds_gcb1 goid:M2E01_BEV1_LD0/LLN0gcb1 timestamp:1614003449.7462208
+WARNING:GooseSim:Channel A: Delayed Packet 0016ms appid:058 st:2317 sq:0000 pdu:gcbref:M2E01_BEV1_LD0/LLN0$GO$gcb1 tal:11000 datset:M2E01_BEV1_LD0/LLN0$ds_gcb1 goid:M2E01_BEV1_LD0/LLN0gcb1 timestamp:1614003449.746354
+WARNING:GooseSim:Channel A: Delayed Packet 0011ms appid:058 st:2318 sq:0001 pdu:gcbref:M2E01_BEV1_LD0/LLN0$GO$gcb1 tal:11000 datset:M2E01_BEV1_LD0/LLN0$ds_gcb1 goid:M2E01_BEV1_LD0/LLN0gcb1 timestamp:1614003449.7464015
+WARNING:GooseSim:Channel A: Delayed Packet 0012ms appid:058 st:2318 sq:0003 pdu:gcbref:M2E01_BEV1_LD0/LLN0$GO$gcb1 tal:11000 datset:M2E01_BEV1_LD0/LLN0$ds_gcb1 goid:M2E01_BEV1_LD0/LLN0gcb1 timestamp:1614003449.7464015
+WARNING:GooseSim:Channel A: Delayed Packet 0020ms appid:058 st:2319 sq:0000 pdu:gcbref:M2E01_BEV1_LD0/LLN0$GO$gcb1 tal:11000 datset:M2E01_BEV1_LD0/LLN0$ds_gcb1 goid:M2E01_BEV1_LD0/LLN0gcb1 timestamp:1614003449.7702327
+WARNING:GooseSim:Channel A: Delayed Packet 0015ms appid:058 st:2319 sq:0001 pdu:gcbref:M2E01_BEV1_LD0/LLN0$GO$gcb1 tal:11000 datset:M2E01_BEV1_LD0/LLN0$ds_gcb1 goid:M2E01_BEV1_LD0/LLN0gcb1 timestamp:1614003449.7702327
+WARNING:GooseSim:Channel A: Delayed Packet 0011ms appid:058 st:2319 sq:0002 pdu:gcbref:M2E01_BEV1_LD0/LLN0$GO$gcb1 tal:11000 datset:M2E01_BEV1_LD0/LLN0$ds_gcb1 goid:M2E01_BEV1_LD0/LLN0gcb1 timestamp:1614003449.7702327
+WARNING:GooseSim:Channel A: Delayed Packet 0029ms appid:058 st:2321 sq:0000 pdu:gcbref:M2E01_BEV1_LD0/LLN0$GO$gcb1 tal:11000 datset:M2E01_BEV1_LD0/LLN0$ds_gcb1 goid:M2E01_BEV1_LD0/LLN0gcb1 timestamp:1614003449.8213756
+WARNING:GooseSim:Channel A: Delayed Packet 0030ms appid:058 st:2322 sq:0000 pdu:gcbref:M2E01_BEV1_LD0/LLN0$GO$gcb1 tal:11000 datset:M2E01_BEV1_LD0/LLN0$ds_gcb1 goid:M2E01_BEV1_LD0/LLN0gcb1 timestamp:1614003449.821419
+WARNING:GooseSim:Channel A: Delayed Packet 0014ms appid:058 st:2322 sq:0001 pdu:gcbref:M2E01_BEV1_LD0/LLN0$GO$gcb1 tal:11000 datset:M2E01_BEV1_LD0/LLN0$ds_gcb1 goid:M2E01_BEV1_LD0/LLN0gcb1 timestamp:1614003449.821419
+WARNING:GooseSim:Channel A: Delayed Packet 0016ms appid:058 st:2322 sq:0002 pdu:gcbref:M2E01_BEV1_LD0/LLN0$GO$gcb1 tal:11000 datset:M2E01_BEV1_LD0/LLN0$ds_gcb1 goid:M2E01_BEV1_LD0/LLN0gcb1 timestamp:1614003449.821419
+WARNING:GooseSim:Channel A: Delayed Packet 0016ms appid:058 st:2322 sq:0004 pdu:gcbref:M2E01_BEV1_LD0/LLN0$GO$gcb1 tal:11000 datset:M2E01_BEV1_LD0/LLN0$ds_gcb1 goid:M2E01_BEV1_LD0/LLN0gcb1 timestamp:1614003449.821419
+WARNING:GooseSim:Channel A: Delayed Packet 0018ms appid:058 st:2323 sq:0000 pdu:gcbref:M2E01_BEV1_LD0/LLN0$GO$gcb1 tal:11000 datset:M2E01_BEV1_LD0/LLN0$ds_gcb1 goid:M2E01_BEV1_LD0/LLN0gcb1 timestamp:1614003449.8932483
+WARNING:GooseSim:Channel A: Delayed Packet 0027ms appid:058 st:2324 sq:0000 pdu:gcbref:M2E01_BEV1_LD0/LLN0$GO$gcb1 tal:11000 datset:M2E01_BEV1_LD0/LLN0$ds_gcb1 goid:M2E01_BEV1_LD0/LLN0gcb1 timestamp:1614003449.8933744
+WARNING:GooseSim:Channel A: Delayed Packet 0033ms appid:058 st:2325 sq:0000 pdu:gcbref:M2E01_BEV1_LD0/LLN0$GO$gcb1 tal:11000 datset:M2E01_BEV1_LD0/LLN0$ds_gcb1 goid:M2E01_BEV1_LD0/LLN0gcb1 timestamp:1614003449.893419
+WARNING:GooseSim:Channel A: Delayed Packet 0023ms appid:058 st:2325 sq:0001 pdu:gcbref:M2E01_BEV1_LD0/LLN0$GO$gcb1 tal:11000 datset:M2E01_BEV1_LD0/LLN0$ds_gcb1 goid:M2E01_BEV1_LD0/LLN0gcb1 timestamp:1614003449.893419
+WARNING:GooseSim:Channel A: Delayed Packet 0021ms appid:058 st:2325 sq:0002 pdu:gcbref:M2E01_BEV1_LD0/LLN0$GO$gcb1 tal:11000 datset:M2E01_BEV1_LD0/LLN0$ds_gcb1 goid:M2E01_BEV1_LD0/LLN0gcb1 timestamp:1614003449.893419
+INFO:GooseSim:Succesfully Played test.pcapng, LAN A: 150 telegrams, LAN B: 150, telegrams
+INFO:GooseSim:Total Delay for Channel A: 571.566104888916 ms
+INFO:GooseSim:Total Delay for Channel B: 148.20122718811035 ms
+```
+
+Both linux and Windows is supported, but linux plays better since python can directly write raw packets withut the need of pcap.
+The performance is not optimized, it may lag here and there.
+VLAN tags are not supported.
+This tool may crash anytime
+Works with python3 should work with python2 as well but not tested.
